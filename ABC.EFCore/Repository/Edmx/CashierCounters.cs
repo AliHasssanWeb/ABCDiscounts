e9091fb465706f9cc160502ca40a57061e37019d
ali Hassan <a.hassan13013@outlook.com>
2023-07-13 16:52:40 +0500

    missing file CashierCounters

---
+++ ABC.EFCore/Repository/Edmx/CashierCounters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC.EFCore.Repository.Edmx
+{
+    public class CashierCounters
+    {
+        [NotMapped]
+        public int OnlineOrders { get; set; }
+
+        [NotMapped]
+        public int TodaysSale { get; set; }
+    }
+}
